1d3a9f88d4577e28d38ce1bc8c4f1fd76af1c47f
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-04-20 14:03:45 +0530

    Changed cors placement

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,7 @@ const fs = require("fs");
 const { exec, spawn } = require("child_process");
 
 console.log(db_URI);
-app.use(
-  cors({
-    origin: "https://lets-code-new-frontend.vercel.app",
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true,
-  }),
-);
+
 app.use(bodyParser.json());
 const email = "sameervohra2004@gmail.com";
 
@@ -72,6 +66,13 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+app.use(
+  cors({
+    origin: "https://lets-code-new-frontend.vercel.app",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.get("/helloworld", (req, res) => {
   console.log("Hello world");
   res.send("Hello world");
