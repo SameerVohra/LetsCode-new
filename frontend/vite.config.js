61089a1044a57aa3da7ffc11dce04d4b51be5140
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-04-21 16:29:06 +0530

    link change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api":
-        "https://lets-code-new-back-2sbsb8fr3-sameervohras-projects.vercel.app",
+        "https://lets-code-new-back-2sbsb8fr3-sameervohras-projects.vercel.app/",
     },
   },
   plugins: [react()],
