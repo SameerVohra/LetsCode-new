cbc42a2d4028188d844460734080d5f531603e27
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-04-22 10:28:18 +0530

    changed the target code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://lets-code-new-back.vercel.app",
         changeOrigin: true,
       },
     },
