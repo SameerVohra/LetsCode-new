843439a677723973fa627d5ae3df40610eb1df3f
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-04-22 18:47:02 +0530

    necessary changes for cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://letscode-new-backend.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
   plugins: [react()],
 });
